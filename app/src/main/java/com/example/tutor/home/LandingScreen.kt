44e4fb4ac25080c7e37e5815ea7eec051261d997
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-30 01:03:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tutor/home/LandingScreen.kt
@@ -0,0 +1,12 @@
+package com.example.tutor.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tutor.R
+
+class LandingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_screen)
+    }
+}
